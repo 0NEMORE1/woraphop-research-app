9dbf0b4a8d4a164c45af5a0a7f5f06271aa4924b
0NEMORE1 <addtessarak@gmail.com>
2025-12-19 09:45:51 +0700

    Update research_app1.py

--- research_app1.py
+++ research_app1.py
@@ -578,7 +578,7 @@ def display_html_metrics(period, radius, temp, insol, snr, dens, ptype, sptype, 
 # 6. UI LOGIC (MAIN)
 # ==============================================================================
 with st.sidebar:
-    st.title("🌌 WORAPHOP AI")
+    st.title("🌌AI Exoplanet Hunter")
     st.caption("EXPLORER EDITION")
     mode = st.radio("Mode:", ["🔬 Single Analysis", "🤖 Deep Sky Crawler"])
     st.markdown("---")
@@ -725,5 +725,6 @@ elif mode == "🤖 Deep Sky Crawler":
                     
 
             st.success("Done.")
+
 
 
